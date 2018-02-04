e0904abc3f4687fc8a13c1e67295a9d5ce7e80b5
Alicia Zaludova <aliciazaludova@gmail.com>
2018-02-04 11:44:59 -0600

    first idea

---
+++ cashToCoins.js
@@ -0,0 +1,20 @@
+const dollarAmount = 0.10
+const piggyBank = {}
+
+// Your magic code here--------
+// i think i want to approach this by multiplying each value by 1. IF it === .10 then it will populate the dimes value. IF NOT === .10 it will evaluate to 0, and it may need to be a string?.
+let quarters = .25;
+let dimes = .10;
+let nickels = .05;
+let pennies = .01; 
+
+// -----------------------------
+console.log(piggyBank)
+
+/* desired output:
+{
+    quarters: 0,
+    dimes: 1,
+    nickels: 0,
+    pennies: 0
+  } */
